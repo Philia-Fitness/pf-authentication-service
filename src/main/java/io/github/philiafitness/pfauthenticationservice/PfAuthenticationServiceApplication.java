713c918a60aa222128fe80b9ba022256f4b4e678
GitlabCiCd <antonio.russi.consulting@gmail.com>
2025-09-22 12:49:58 +0200

    [ADD] - Initial development

---
+++ src/main/java/io/github/philiafitness/pfauthenticationservice/PfAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.philiafitness.pfauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfAuthenticationServiceApplication.class, args);
+	}
+
+}
